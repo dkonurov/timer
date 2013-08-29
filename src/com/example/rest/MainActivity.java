c7e8e78e7fcf6dba229b13de7646d19d1ad7a6dc
Dmitry Konurov <dmitry.konurov@opensoftdev.ru>
2013-08-29 16:30:27 +0400

    add button
    Signed-off-by: Dmitry Konurov <dmitry.konurov@opensoftdev.ru>

--- src/com/example/rest/MainActivity.java
+++ src/com/example/rest/MainActivity.java
@@ -16,7 +16,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
@@ -39,8 +38,10 @@ public class MainActivity extends Activity implements OnClickListener {
 	private static int END_TIME = 2;
 	private static int PERIODIC_TIME = 4;
 	private int check = 0;
-	private int myHour = 0;
-	private int myMinute = 0;
+	private int setHour = 0;
+	private int setMinute = 0;
+	private int periodicHour = 0;
+	private int periodicMinute = 0;
 	
 	private String SAVED_TEXT = "saved check";
 
@@ -68,21 +69,40 @@ public class MainActivity extends Activity implements OnClickListener {
 		loadCheck();
 		
 		for (int i=0;i<=5;i++) {
-			if(i<=1) {
-				count = START_TIME;
-			} else if (i<=3) {
-				count = END_TIME;
-			} else {
-				count = PERIODIC_TIME;
-			}
 			Log.d(LOG_TAG, "i ="+i);
 			timer[i].setOnTouchListener(new OnTouchListener(){
-				final int id = count;
 				@SuppressWarnings("deprecation")
 				@Override
 				public boolean onTouch(View arg0, MotionEvent arg1) {
 					// TODO Auto-generated method stub
-					showDialog(id);
+					
+					switch(arg0.getId()){
+						case R.id.startHours:
+							showDialog(START_TIME);
+							count=START_TIME;
+							break;
+						case R.id.startMinute:
+							showDialog(START_TIME);
+							count = START_TIME;
+							break;
+						case R.id.endHours:
+							showDialog(END_TIME);
+							count = END_TIME;
+							break;
+						case R.id.endMinute:
+							showDialog(END_TIME);
+							count = END_TIME;
+							break;
+						case R.id.periodicHours:
+							showDialog(PERIODIC_TIME);
+							count = PERIODIC_TIME;
+							break;
+						case R.id.periodicMinute:
+							showDialog(PERIODIC_TIME);
+							count = PERIODIC_TIME;
+							break;
+					}
+					
 					return false;
 				}
 			});
@@ -109,42 +129,34 @@ public class MainActivity extends Activity implements OnClickListener {
 		
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch(v.getId()) {
 			case R.id.set:
-				InputMethodManager imm = (InputMethodManager)getSystemService(
-					      Context.INPUT_METHOD_SERVICE);
-					for (int i=0; i<=5;i++) {
-						imm.hideSoftInputFromWindow(timer[i].getWindowToken(), 0);
-					}
-					
 				if (check == 0) {
 					String[] stringTimer = new String[6];
 					int[] intTimer = new int[6];
+					boolean chekerTime = true;
 					for (int i=0;i<=5;i++) {
 						stringTimer[i] = timer[i].getText().toString();
 						if (stringTimer[i].length() == 0) {
 							Toast.makeText(this, "все поля должны быть заполнены", Toast.LENGTH_SHORT).show();
+							chekerTime = false;
+							break;
 						} else {
 							intTimer[i] = Integer.valueOf(stringTimer[i]);
-							if (i%2 == 0) {
-								if (intTimer[i] > 23) {
-									Toast.makeText(this, "Не правильное время", Toast.LENGTH_SHORT).show();
-									timer[i].setText("");
-									timer[i].requestFocus();
-									return;
-								}
-							} else {
-								if (intTimer[i] > 60) {
-									Toast.makeText(this, "не правильное время", Toast.LENGTH_SHORT).show();
-									timer[i].setText("");
-									timer[i].requestFocus();
-									return;
-								}
+							if (i == 5 && intTimer[4] == 0 && intTimer[5] == 0) {
+								Toast.makeText(this, "должны быть время между отдыхом", Toast.LENGTH_LONG).show();
+								showDialog(PERIODIC_TIME);
+								chekerTime = false;
+								break;
 							}
 						}
+					}
+					if (!chekerTime) {
+						return;
 					}
 					installTimer.setText("Остановить таймер");
 					Context context = this.getApplicationContext();
@@ -164,22 +176,20 @@ public class MainActivity extends Activity implements OnClickListener {
 	
 	@SuppressWarnings("deprecation")
 	protected Dialog onCreateDialog(int id) {
-		if (id == START_TIME || id == END_TIME || id == PERIODIC_TIME) {
-			count = id;
-			TimePickerDialog tpd = new TimePickerDialog(this, myCallBack, myHour, myMinute, true);
+		if (id == START_TIME || id == END_TIME) {
+			TimePickerDialog tpd = new TimePickerDialog(this, myCallBack, setHour, setMinute, true);
 			return tpd;
-	}
+		} else if (id == PERIODIC_TIME) {
+			TimePickerDialog tpd = new TimePickerDialog(this, myCallBack, periodicHour, periodicMinute, true);
+			return tpd;
+		}
 		return super.onCreateDialog(id);
 	}
 	
 	 OnTimeSetListener myCallBack = new OnTimeSetListener() {
 		    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-		    	final int id = count;
-		    	Log.d(LOG_TAG, "id ="+id);
-		      myHour = hourOfDay;
-		      myMinute = minute; 
-		      timer[id].setText(myHour+"");
-		      timer[id+1].setText(myMinute+"");
+		      timer[count].setText(hourOfDay+"");
+		      timer[count+1].setText(minute+"");
 		    }
 	 };
 	
